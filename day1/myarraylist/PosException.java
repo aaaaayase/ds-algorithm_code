ff66082bc38e1cd850a9d4b59a8021f4e470cdce
dcy <2284858868@qq.com>
2023-11-26 22:29:35 +0800

    实现ArrayList

---
+++ day1/myarraylist/PosException.java
@@ -0,0 +1,11 @@
+package day1.myarraylist;
+
+public class PosException extends RuntimeException {
+    public PosException() {
+
+    }
+
+    public PosException(String str) {
+        super(str);
+    }
+}
