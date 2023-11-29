28465a72654cc5d53a650ae625e18f2316c3b69f
dcy <2284858868@qq.com>
2023-11-29 09:51:55 +0800

    实现单链表

---
+++ day2/mysinglelist/MySingleListIndexOutOfException.java
@@ -0,0 +1,11 @@
+package day2.mysinglelist;
+
+public class MySingleListIndexOutOfException extends RuntimeException {
+    public MySingleListIndexOutOfException() {
+
+    }
+
+    public MySingleListIndexOutOfException(String str) {
+        super(str);
+    }
+}
