1e3e911aaf2c464aa6076d8faf6b671c39ed7f40
dcy <2284858868@qq.com>
2023-11-29 10:04:13 +0800

    删除链表中所有指定值的元素

---
+++ day2/demo1.java
@@ -0,0 +1,41 @@
+package day2;
+
+public class demo1 {
+    static class ListNode {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    /**
+     * 删除链表中所有值为value的元素
+     * @param head
+     * @param val
+     * @return
+     */
+    public ListNode removeElements(ListNode head, int val) {
+        if (head == null) {
+            return head;
+        }
+        ListNode pre = head;
+        ListNode cur = head.next;
+
+        while (cur != null) {
+            if (cur.val == val) {
+                pre.next = cur.next;
+                cur = cur.next;
+            } else {
+                pre = cur;
+                cur = cur.next;
+            }
+
+        }
+        if (head.val == val) {
+            head = head.next;
+        }
+        return head;
+    }
+}
