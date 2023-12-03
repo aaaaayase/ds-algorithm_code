6ac04ef24f5edf3256093428bcb279f6329667aa
dcy <2284858868@qq.com>
2023-12-03 09:25:48 +0800

    数组实现循环队列

---
+++ day5/mycircularqueue/MyCircularQueue.java
@@ -0,0 +1,60 @@
+package day5.mycircularqueue;
+
+//设计循环队列
+
+
+//设计循环队列
+public class MyCircularQueue {
+
+    int[] array;
+    int front;
+    int rear;
+
+
+    public MyCircularQueue(int k) {
+        array = new int[k + 1];
+
+    }
+
+    public boolean enQueue(int value) {
+        if (isFull()) {
+            return false;
+        }
+        array[rear] = value;
+        rear = (rear + 1) % array.length;
+
+
+        return true;
+    }
+
+    public boolean deQueue() {
+        if (isEmpty()) {
+            return false;
+        }
+        front = (front + 1) % array.length;
+        return true;
+
+    }
+
+    public int Front() {
+        if(isEmpty()) {
+            return -1;
+        }
+        return array[front];
+    }
+
+    public int Rear() {
+        if(isEmpty()) {
+            return -1;
+        }
+        return rear == 0 ? array[array.length - 1] : array[rear - 1];
+    }
+
+    public boolean isEmpty() {
+        return rear == front;
+    }
+
+    public boolean isFull() {
+        return (rear + 1) % array.length == front;
+    }
+}
