0ab3851014b0f1d58cef700d39f896aa8ee75bd8
dcy <2284858868@qq.com>
2023-12-03 11:12:44 +0800

    实现最小栈

---
+++ day5/minstack/MinStack.java
@@ -0,0 +1,52 @@
+package day5.minstack;
+
+import java.util.Stack;
+
+public class MinStack {
+    Stack<Integer> stack;
+    Stack<Integer> minStack;
+
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int val) {
+        if (stack.empty()) {
+            stack.push(val);
+            minStack.push(val);
+        } else {
+            stack.push(val);
+            if (val<=minStack.peek()) {
+                minStack.push(val);
+            }
+        }
+    }
+
+    public void pop() {
+        if (stack.empty()) {
+            return ;
+        }
+
+        int temp=stack.pop();
+        if (minStack.peek()==temp) {
+            minStack.pop();
+        }
+
+    }
+
+    public int top() {
+        if (stack.empty()) {
+            return -1;
+        }
+        return stack.peek();
+    }
+
+    public int getMin() {
+        if (stack.empty()) {
+            return -1;
+        }
+        return minStack.peek();
+    }
+
+}
