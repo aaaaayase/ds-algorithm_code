3dff1c9129cea40a12e85f1d5e42edbaf5e771d1
dcy <2284858868@qq.com>
2023-12-08 21:47:10 +0800

    递归实现树节点的遍历

---
+++ day6/binarytree/BinaryTree.java
@@ -0,0 +1,55 @@
+package day6.binarytree;
+
+public class BinaryTree {
+
+    static class TreeNode {
+        char val;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode(char val) {
+            this.val = val;
+        }
+    }
+
+    //后序遍历
+    public void postOrder(TreeNode root) {
+
+        if (root==null) {
+            return ;
+        }
+
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.print(root.val+" ");
+
+    }
+
+    //中序遍历
+    public void inOrder(TreeNode root) {
+
+        if (root==null) {
+            return ;
+        }
+
+        postOrder(root.left);
+        System.out.print(root.val+" ");
+        postOrder(root.right);
+
+
+    }
+
+    //先序遍历
+    public void preOrder(TreeNode root) {
+
+        if (root==null) {
+            return ;
+        }
+
+        System.out.print(root.val+" ");
+        postOrder(root.left);
+        postOrder(root.right);
+
+
+    }
+}
