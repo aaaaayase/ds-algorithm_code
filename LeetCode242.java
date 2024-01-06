333c1c7ad90a9d8fa770ff91d481beb61f838fd5
dcy <2284858868@qq.com>
2024-01-06 22:22:11 +0800

    242. 有效的字母异位词

---
+++ LeetCode242.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class LeetCode242 {
+    //给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
+    //
+    //注意：若 s 和 t 中每个字符出现的次数都相同，则称 s 和 t 互为字母异位词。
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()) {
+            return false;
+        }
+
+        char[] str1=s.toCharArray();
+        char[] str2=t.toCharArray();
+
+        Arrays.sort(str1);
+        Arrays.sort(str2);
+        return Arrays.equals(str1,str2);
+    }
+}
