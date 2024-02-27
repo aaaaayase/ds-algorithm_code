69a09619c5afd0d66a572f65cb8ba1c320165330
dcy <2284858868@qq.com>
2024-02-27 22:09:37 +0800

    226.翻转二叉树

---
+++ 二叉树/LeetCode226.java
@@ -0,0 +1,31 @@
+public class LeetCode226 {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode() {
+            this.val = val;
+        }
+
+    }
+
+    public TreeNode invertTree(TreeNode root) {
+
+        if (root == null) {
+            return null;
+        }
+
+        if (root.right == null && root.left == null) {
+            return root;
+        }
+
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+}
