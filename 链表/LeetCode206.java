3a1ea6374c55c3876446bb95c81feb8f7accd36f
dcy <2284858868@qq.com>
2024-02-27 22:28:48 +0800

    206. 反转链表

---
+++ 链表/LeetCode206.java
@@ -0,0 +1,31 @@
+public class LeetCode206 {
+
+    private static class ListNode {
+        public int val;
+        public ListNode next;
+
+        public ListNode() {
+        }
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    //给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+    public ListNode reverseList(ListNode head) {
+        if(head==null) {
+            return null;
+        }
+
+        ListNode temp=new ListNode();
+        ListNode cur=head;
+        while(cur!=null) {
+            ListNode curNext=cur.next;
+            cur.next=temp.next;
+            temp.next=cur;
+            cur=curNext;
+        }
+        return temp.next;
+    }
+}
