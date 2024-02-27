f67077a8b2f4ed613e4cc10840bbfa9b56d52b8e
dcy <2284858868@qq.com>
2024-02-27 22:26:24 +0800

    83. 删除排序链表中的重复元素

---
+++ 链表/LeetCode83.java
@@ -0,0 +1,40 @@
+public class LeetCode83 {
+
+    private static class ListNode {
+        public int val;
+        public ListNode next;
+
+        public ListNode() {
+        }
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    //给定一个已排序的链表的头 head ， 删除所有重复的元素，使每个元素只出现一次 。返回 已排序的链表 。
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head==null) {
+            return null;
+        }
+        ListNode cur=head.next;
+        ListNode pre=head;
+        while(cur!=null) {
+
+            if(cur.val==pre.val) {
+                ListNode curNext=cur.next;
+                pre.next=curNext;
+                cur=curNext;
+            } else {
+                pre=cur;
+                cur=cur.next;
+            }
+
+
+        }
+
+
+
+        return head;
+    }
+}
