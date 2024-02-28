d7d41f0faf4764164ba0cd16858eb196f1d8bb28
dcy <2284858868@qq.com>
2024-02-28 23:10:19 +0800

    61. 旋转链表

---
+++ 链表/LeetCode61.java
@@ -0,0 +1,54 @@
+public class LeetCode61 {
+
+    private static class ListNode {
+        public int val;
+        public ListNode next;
+
+        public ListNode() {
+        }
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public ListNode rotateRight(ListNode head, int k) {
+        if(head==null) {
+            return null;
+        }
+        int len=0;
+        ListNode temp=head;
+        while(temp!=null) {
+            temp=temp.next;
+            len++;
+        }
+        if(k%len==0) {
+            return head;
+        }
+        ListNode pre=findNode(head,k%len+1);
+        ListNode cur=findNode(head,k%len);
+        ListNode tail=findNode(head,1);
+        pre.next=null;
+        tail.next=head;
+        return cur;
+    }
+
+
+    public ListNode findNode(ListNode head, int k) {
+        if(head==null) {
+            return null;
+        }
+        ListNode fast=head;
+        ListNode slow=head;
+        while(k!=1) {
+            fast=fast.next;
+            k--;
+        }
+        while(fast.next!=null) {
+            fast=fast.next;
+            slow=slow.next;
+
+        }
+        return slow;
+    }
+}
