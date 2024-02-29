157a39e695573fabf6e9e769fbb0313111247cb9
dcy <2284858868@qq.com>
2024-02-29 22:36:35 +0800

    链表的基本操作实现

---
+++ 链表/mysinglelist/MySingleListIndexOutOfException.java
@@ -0,0 +1,11 @@
+package mysinglelist;
+
+public class MySingleListIndexOutOfException extends RuntimeException {
+    public MySingleListIndexOutOfException() {
+
+    }
+
+    public MySingleListIndexOutOfException(String str) {
+        super(str);
+    }
+}
