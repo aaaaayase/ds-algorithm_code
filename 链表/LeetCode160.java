37bf2258c86a9817473e5d120e66eb253d1a3eca
dcy <2284858868@qq.com>
2024-03-13 20:32:58 +0800

    160. 相交链表

---
+++ 链表/LeetCode160.java
@@ -0,0 +1,53 @@
+public class LeetCode160 {
+    private static class ListNode {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    //给你两个单链表的头节点 headA 和 headB ，
+    //请你找出并返回两个单链表相交的起始节点。
+    //如果两个链表不存在相交节点，返回 null。
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        int lenA = 0, lenB = 0;
+        ListNode curA = headA;
+        ListNode curB = headB;
+        while (curA != null) {
+            curA = curA.next;
+            lenA++;
+        }
+        while (curB != null) {
+            curB = curB.next;
+            lenB++;
+        }
+
+        int len = lenA - lenB;
+        ListNode listL = headA;
+        ListNode listS = headB;
+        if (len < 0) {
+            len = lenB - lenA;
+            listL = headB;
+            listS = headA;
+        }
+
+        while (len != 0) {
+            listL = listL.next;
+            len--;
+        }
+
+        while (listS != null && listL != null && listL != listS) {
+            listL = listL.next;
+            listS = listS.next;
+        }
+
+        if (listL == listS && listL == null) {
+            return null;
+        }
+
+        return listL;
+    }
+
+}
