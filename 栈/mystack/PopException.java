e1332b14f03ccf5da680ce944b5bb9e617601721
dcy <2284858868@qq.com>
2024-03-14 14:57:25 +0800

    栈的基本操作实现

---
+++ 栈/mystack/PopException.java
@@ -0,0 +1,11 @@
+package mystack;
+
+public class PopException extends RuntimeException {
+    public PopException() {
+        super();
+    }
+
+    public PopException(String message) {
+        super(message);
+    }
+}
