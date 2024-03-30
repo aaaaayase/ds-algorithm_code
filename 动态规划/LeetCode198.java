91806b144127222ecaaf221ce0e4ea9e5a43bef1
dcy <2284858868@qq.com>
2024-03-30 23:39:42 +0800

    198. 打家劫舍

---
+++ 动态规划/LeetCode198.java
@@ -0,0 +1,18 @@
+public class LeetCode198 {
+    public int rob(int[] nums) {
+        int n = nums.length;
+
+        int[] f = new int[n];
+        int[] g = new int[n];
+
+
+        f[0] = nums[0];
+
+        for (int i = 1; i < n; i++) {
+            f[i] = g[i - 1] + nums[i];
+            g[i] = Math.max(g[i - 1], f[i - 1]);
+        }
+
+        return Math.max(f[n - 1], g[n - 1]);
+    }
+}
