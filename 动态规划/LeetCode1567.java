a35e7387b0e21f822c32d8be6eac587954d9d886
dcy <2284858868@qq.com>
2024-04-02 23:50:03 +0800

    1567. 乘积为正数的最长子数组长度

---
+++ 动态规划/LeetCode1567.java
@@ -0,0 +1,30 @@
+public class LeetCode1567 {
+
+    public int getMaxLen(int[] nums) {
+        int n = nums.length;
+
+        int[] f = new int[n + 1];
+        int[] g = new int[n + 1];
+
+
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 1; i <= n; i++) {
+            if (nums[i - 1] > 0) {
+                f[i] = f[i - 1] + 1;
+                g[i] = g[i - 1] == 0 ? 0 : g[i - 1] + 1;
+            } else if (nums[i - 1] < 0) {
+                f[i] = g[i - 1] == 0 ? 0 : g[i - 1] + 1;
+                g[i] = f[i - 1] + 1;
+            }
+
+            if (f[i] > max) {
+                max = f[i];
+            }
+        }
+
+        return max;
+    }
+
+
+}
