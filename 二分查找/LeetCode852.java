f2bdd35fc2a2fffdbe86f011966c9772656e6fce
ColdGoose77 <108540264+ColdGoose77@users.noreply.github.com>
2024-07-03 23:01:37 +0800

    852. 山脉数组的峰顶索引

--- 二分查找/LeetCode852.java
+++ 二分查找/LeetCode852.java
@@ -3,7 +3,7 @@ public class LeetCode852 {
         int left = 1, right = arr.length - 2;
         while (left < right) {
             int mid = left + (right - left + 1) / 2;
-            if (arr[mid] >= arr[mid - 1]) {
+            if (arr[mid] > arr[mid - 1]) {
                 left = mid;
             } else {
                 right = mid - 1;
