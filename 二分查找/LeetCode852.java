72d3d69a3d44c185fd9d688f590879adbeec12d8
dcy <2284858868@qq.com>
2024-07-03 21:39:41 +0800

    852. 山脉数组的峰顶索引

---
+++ 二分查找/LeetCode852.java
@@ -0,0 +1,14 @@
+public class LeetCode852 {
+    public int peakIndexInMountainArray(int[] arr) {
+        int left = 1, right = arr.length - 2;
+        while (left < right) {
+            int mid = left + (right - left + 1) / 2;
+            if (arr[mid] >= arr[mid - 1]) {
+                left = mid;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+}
