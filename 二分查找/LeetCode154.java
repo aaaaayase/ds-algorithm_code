07db0de2a207319c88192eb28d0582d1efad9e9d
dcy <2284858868@qq.com>
2024-07-07 23:32:40 +0800

    154. 寻找旋转排序数组中的最小值 II

---
+++ 二分查找/LeetCode154.java
@@ -0,0 +1,16 @@
+public class LeetCode154 {
+    public int findMin(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else if (nums[mid] < nums[right]) {
+                right = mid;
+            } else {
+                right -= 1;
+            }
+        }
+        return nums[right];
+    }
+}
