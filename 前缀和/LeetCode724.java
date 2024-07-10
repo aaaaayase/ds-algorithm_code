196b9add0cd0feea14a763cfe46b82b49c8452d4
dcy <2284858868@qq.com>
2024-07-10 22:34:43 +0800

    724. 寻找数组的中心下标

---
+++ 前缀和/LeetCode724.java
@@ -0,0 +1,23 @@
+public class LeetCode724 {
+    public int pivotIndex(int[] nums) {
+        int n = nums.length;
+        int[] f = new int[n];
+        int[] g = new int[n];
+
+        for (int i = 1; i < n; i++) {
+            f[i] = f[i - 1] + nums[i - 1];
+        }
+
+        for (int i = n - 2; i >= 0; i--) {
+            g[i] = g[i + 1] + nums[i + 1];
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (f[i] == g[i]) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
