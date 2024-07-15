4001b1616b55903883e0bec1ae64c9c4cccb4dce
dcy <2284858868@qq.com>
2024-07-15 14:34:03 +0800

    136. 只出现一次的数字

---
+++ 位运算/LeetCode136.java
@@ -0,0 +1,10 @@
+public class LeetCode136 {
+    public int singleNumber(int[] nums) {
+        int ret = 0;
+        for (int i = 0; i < nums.length; i++) {
+            ret ^= nums[i];
+        }
+
+        return ret;
+    }
+}
