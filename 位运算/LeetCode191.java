30658e5c7bc308279bbec8b43bfb45aeb940faa8
dcy <2284858868@qq.com>
2024-07-15 11:51:52 +0800

    191. 位1的个数

---
+++ 位运算/LeetCode191.java
@@ -0,0 +1,12 @@
+public class LeetCode191 {
+    public int hammingWeight(int n) {
+        int count = 0;
+        while (n != 0) {
+            if ((n | 1) == n) {
+                count++;
+            }
+            n = (n >> 1);
+        }
+        return count;
+    }
+}
