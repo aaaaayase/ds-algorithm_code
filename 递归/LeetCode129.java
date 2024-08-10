f18a1c368bba90a8ac5f4c4040d6dd275cc66318
ColdGoose77 <108540264+ColdGoose77@users.noreply.github.com>
2024-08-10 10:54:44 +0800

    129. 求根节点到叶节点数字之和

--- 递归/LeetCode129.java
+++ 递归/LeetCode129.java
@@ -4,17 +4,19 @@ public class LeetCode129 {
     }
 
     public int dfs(TreeNode root, int preSum) {
-        if (root == null) {
-            return 0;
-        }
         preSum = preSum * 10 + root.val;
         if (root.left == null && root.right == null) {
             return preSum;
         }
 
-        int left = dfs(root.left, preSum);
-        int right = dfs(root.right, preSum);
+        int ret = 0;
+        if (root.left != null) {
+            ret += dfs(root.left, preSum);
+        }
+        if (root.right != null) {
+            ret += dfs(root.right, preSum);
+        }
 
-        return left + right;
+        return ret;
     }
 }
