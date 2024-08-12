fe7244dbcb3d14b8a13775379ffa500fedcca029
ColdGoose77 <108540264+ColdGoose77@users.noreply.github.com>
2024-08-13 00:07:46 +0800

    219. 存在重复元素 II

--- Map&Set/LeetCode219.java
+++ Map&Set/LeetCode219.java
@@ -1,22 +1,16 @@
 import java.util.HashMap;
 import java.util.Map;
 
-public class LeetCode219 {
-    //给你一个整数数组 nums 和一个整数 k ，判断数组中是否存在两个 不同的索引 i 和 j ，满足 nums[i] == nums[j] 且 abs(i - j) <= k 。
-    // 如果存在，返回 true ；否则，返回 false 。
+public class LeetCode217 {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        Map<Integer, Integer> map = new HashMap<>();
-
+        Map<Integer, Integer> hash = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            if (!map.containsKey(nums[i])) {
-                map.put(nums[i], i);
-            } else {
-                if(Math.abs(map.get(nums[i])-i)<=k) {
-                    return true;
-                }
-                map.put(nums[i],i);
+            if (hash.containsKey(nums[i]) && Math.abs(i - hash.get(nums[i])) <= k) {
+                return true;
             }
+            hash.put(nums[i], i);
         }
+
         return false;
     }
 }
