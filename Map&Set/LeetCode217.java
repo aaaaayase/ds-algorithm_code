0bd7ab11f20d864a1f100f31bef1aaf8f88a7396
ColdGoose77 <108540264+ColdGoose77@users.noreply.github.com>
2024-08-13 00:00:08 +0800

    217. 存在重复元素

--- Map&Set/LeetCode217.java
+++ Map&Set/LeetCode217.java
@@ -1,28 +1,16 @@
-import java.util.*;
+import java.util.HashSet;
+import java.util.Set;
 
 public class LeetCode217 {
-    //给你一个整数数组 nums 。如果任一值在数组中出现 至少两次 ，返回 true ；
-    // 如果数组中每个元素互不相同，返回 false 。
     public boolean containsDuplicate(int[] nums) {
-        Map<Integer,Integer> map=new HashMap<>();
-
-        for (int num : nums) {
-            if(!map.containsKey(num)) {
-                map.put(num,1);
-            } else {
-                map.put(num,map.get(num)+1);
-            }
-        }
-
-
-        List<Integer> list = new ArrayList<>(map.values());
-
-        for (Integer num:
-                list) {
-            if(num>1) {
+        Set<Integer> hash = new HashSet<>();
+        for (int x : nums) {
+            if (hash.contains(x)) {
                 return true;
             }
+            hash.add(x);
         }
+
         return false;
     }
 }
