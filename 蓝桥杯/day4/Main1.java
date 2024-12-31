e94be97ce6bdaf2203314f3b6b97210b74d87e25
aaaaayase <2284858868@qq.com>
2024-12-31 09:31:03 +0800

    P11041 [蓝桥杯 2024 省 Java B] 报数游戏

---
+++ 蓝桥杯/day4/Main1.java
@@ -0,0 +1,10 @@
+/**
+ * @author yun
+ * @date 2024/12/31 9:29
+ * @desciption: 报数游戏
+ */
+public class Main1 {
+    public static void main(String[] args) {
+        System.out.println(12*202420242024L);
+    }
+}
